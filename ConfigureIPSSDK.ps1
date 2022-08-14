d9fc1ebe6b1e80bdcce8d1891e63b074c31755bd
sadgene <89575813+sadgene@users.noreply.github.com>
2022-08-14 23:35:44 +0300

    Delete ConfigureIPSSDK.ps1

--- ConfigureIPSSDK.ps1
+++
@@ -1,8 +0,0 @@
-# Этот скрипт конфигурирует IPS SDK
-
-
-# переменные окружения, используемые в IPS SDK
-# TODO: укажите пути к каталогам сервера приложений и клиента IPS
-$env:IPS_VERSION = 6
-$env:IPS_SERVER_DIR = Resolve-Path "\\ta03\c`$\Program Files\IPS\Server"
-$env:IPS_CLIENT_DIR = Resolve-Path "C:\Program Files\IPS03\Client"
